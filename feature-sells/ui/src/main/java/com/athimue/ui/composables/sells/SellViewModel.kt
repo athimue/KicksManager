4d0840a9caf217ebe50af550b750a094495b5a2b
athimue <84283744+athimue@users.noreply.github.com>
2023-11-17 18:06:05 +0100

    impr(sells): add dispatchers inside viewModel to not block the UI thread

--- feature-sells/ui/src/main/java/com/athimue/ui/composables/sells/SellViewModel.kt
+++ feature-sells/ui/src/main/java/com/athimue/ui/composables/sells/SellViewModel.kt
@@ -5,28 +5,31 @@ import androidx.lifecycle.viewModelScope
 import com.athimue.domain.usecases.DeleteSellUseCase
 import com.athimue.domain.usecases.GetSellsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class SellViewModel @Inject constructor(
-    private val getSellsUseCase: GetSellsUseCase,
-    private val deleteSellUseCase: DeleteSellUseCase
+    private val getSellsUseCase: GetSellsUseCase, private val deleteSellUseCase: DeleteSellUseCase
 ) : ViewModel() {
 
     var uiState = MutableStateFlow(SellUiState())
 
     init {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             getSellsUseCase.invoke().collect {
-                uiState.value = uiState.value.copy(sells = it.map { it.toSellUiModel() })
+                withContext(Dispatchers.Main) {
+                    uiState.value = uiState.value.copy(sells = it.map { it.toSellUiModel() })
+                }
             }
         }
     }
 
     fun deleteSell(sellId: Long) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             deleteSellUseCase.deleteSell(sellId)
         }
     }
