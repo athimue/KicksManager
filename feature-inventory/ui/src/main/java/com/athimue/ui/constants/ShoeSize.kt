26eae2496af08eb108bd87aaadbcf3e5672ea0ac
athimue <84283744+athimue@users.noreply.github.com>
2023-11-21 16:25:43 +0100

    impr(inventory): create enum of shoes size and shops

---
+++ feature-inventory/ui/src/main/java/com/athimue/ui/constants/ShoeSize.kt
@@ -0,0 +1,17 @@
+package com.athimue.ui.constants
+
+enum class ShoeSize(val size: String) {
+    SIZE_38("38"),
+    SIZE_38_5("38.5"),
+    SIZE_39("39"),
+    SIZE_40("40"),
+    SIZE_40_5("40.5"),
+    SIZE_41("41"),
+    SIZE_42("42"),
+    SIZE_42_5("42.5"),
+    SIZE_42_2_3("42 2/3"),
+    SIZE_43("43"),
+    SIZE_44("44"),
+    SIZE_44_5("44.5"),
+    SIZE_45("45")
+}
---
+++ feature-inventory/ui/src/main/java/com/athimue/ui/constants/Shop.kt
@@ -0,0 +1,19 @@
+package com.athimue.ui.constants
+
+enum class Shop(val shopName: String) {
+    ADIDAS_CONFIRMED("ADIDAS / CONFIRMED"),
+    BSTN("BSTN"),
+    BUZZZ("BUZZZ"),
+    COURIR("COURIR"),
+    FOOT_DISTRICT("FOOT DISTRICT"),
+    GLF("GLF"),
+    IMPACT("IMPACT"),
+    KITH("KITH"),
+    NIKE_SNKRS("NIKE / SNKRS"),
+    PATTA("PATTA"),
+    SNIPES("SNIPES"),
+    SOLEBOX("SOLEBOX"),
+    STARCOW("STARCOW"),
+    SUPREME("SUPREME"),
+    SVD("SVD")
+}
