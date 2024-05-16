0d4cbcbdbad4105e5e40dcce6f711601a65fbb0f
athimue <84283744+athimue@users.noreply.github.com>
2024-05-17 00:35:05 +0200

    feat(trends): add trends feature skeleton

---
+++ feature-trends/ui/src/androidTest/java/com/athimue/feature_trends/ui/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.athimue.feature_trends.ui
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    assertEquals("com.athimue.feature_trends.ui.test", appContext.packageName)
+  }
+}
